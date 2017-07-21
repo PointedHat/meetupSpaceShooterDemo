85659ace00ada32e39b58194cf318ac0830909ee
Steve Ives <steve@steveives.co.uk>
2017-07-21 11:50:00 +0100

    Initial commit, with moving invaders.

--- meetupSpaceShooter/GameScene.swift
+++ meetupSpaceShooter/GameScene.swift
@@ -2,88 +2,78 @@
 //  GameScene.swift
 //  meetupSpaceShooter
 //
-//  Created by Stephen Ives on 21/07/2017.
+//  Created by Stephen Ives on 11/07/2017.
 //  Copyright © 2017 Stephen Ives. All rights reserved.
 //
 
 import SpriteKit
-import GameplayKit
+
+enum AlienDirection {
+    case up, down, leftThenUp, leftThenDown
+}
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var timeOfLastMove: CFTimeInterval = 0.0
+    var timePerMove: CFTimeInterval = 1.0
+    var currentAlienDirection = AlienDirection.up
+    var nextAlienMoveDirection = AlienDirection.up
+    
+    let alienMoveUpDistance = CGVector(dx:0, dy: 25)
+    let alienMoveDownDistance = CGVector(dx:0, dy: -25)
+    let alienMoveLeftDistance = CGVector(dx:-20, dy: 0)
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let viewFrame = view.frame.size
+        print("View's frame is \(viewFrame)")
+        let sceneFrame = frame.size
+        print("Scene's frame is \(sceneFrame)")
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        moveAliensForUpdate(currentTime)
+    }
+    
+    func moveAliensForUpdate(_ currentTime: CFTimeInterval) {
+        if (currentTime - timeOfLastMove < timePerMove) {return}
+        
+        enumerateChildNodes(withName: "//alien") {
+            node, stop in
+            let alien = node as! SKSpriteNode
+            
+            switch self.currentAlienDirection {
+            case .up:
+                alien.run(SKAction.move(by: self.alienMoveUpDistance, duration: 0))
+                if alien.frame.maxY >= (node.scene?.size.height)!/2 {
+                    self.nextAlienMoveDirection = .leftThenDown
+                }
+            case .down:
+                alien.run(SKAction.move(by: self.alienMoveDownDistance, duration: 0))
+                if alien.frame.minY <= -(node.scene?.size.height)!/2 {
+                    self.nextAlienMoveDirection = .leftThenUp
+                }
+            case .leftThenUp, .leftThenDown:
+                alien.run(SKAction.move(by: self.alienMoveLeftDistance, duration: 0))
+                self.nextAlienMoveDirection = self.currentAlienDirection == .leftThenDown ? .down : .up
+            }
+        }
+        self.timeOfLastMove = currentTime
+        if nextAlienMoveDirection != self.currentAlienDirection {
+            currentAlienDirection = nextAlienMoveDirection
+        }
+        
     }
 }
--- meetupSpaceShooter/GameViewController.swift
+++ meetupSpaceShooter/GameViewController.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,24 +30,20 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+            return .landscape
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
